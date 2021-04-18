54ed773871dd4f8b2bb8a671dd17d61f81d354f6
Igor Gassmann <igor@igassmann.me>
2021-04-17 22:01:23 -0300

    refactor: make link https

--- src/components/comment.js
+++ src/components/comment.js
@@ -43,7 +43,7 @@ const Comment = () => (
   <CommentContainer>
     <blockquote>
       <p>This website uses Stir's tech stack and aims to match its coding style. It was based upon <a href="https://usestir.com/">Stir's homepage</a> and it's publicly <a href="https://github.com/IGassmann/stir-pitch-website">available on GitHub</a> to be reviewed.</p>
-      <p><a href="http://nextjs.org/">#nextjs</a> <a href="https://vercel.com/">#vercel</a> <a href="https://styled-components.com/">#styled-components</a> <a href="https://animate.style/">#animate-css</a></p>
+      <p><a href="https://nextjs.org/">#nextjs</a> <a href="https://vercel.com/">#vercel</a> <a href="https://styled-components.com/">#styled-components</a> <a href="https://animate.style/">#animate-css</a></p>
     </blockquote>
   </CommentContainer>
 );
