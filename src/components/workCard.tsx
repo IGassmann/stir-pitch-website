c258991e8381f10c42fd2266014f1dee37efe8ae
Igor Gassmann <igor@igassmann.me>
2021-04-18 19:34:29 -0300

    fix: work card title on mobile

--- src/components/workCard.tsx
+++ src/components/workCard.tsx
@@ -4,32 +4,38 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 const WorkCardListItem = styled.li`
-    margin: 50px 30px;
-    display: block;
-    border-radius: 16px;
+  margin: 50px 30px;
+  display: block;
+  border-radius: 16px;
 
-    h3 {
-      font-weight: normal;
-      font-size: 20px;
-      line-height: 110%;
-      margin: 20px 0;
-      text-align: left;
-      
-      span {
-        background-color: rgb(232, 147, 204);
-        background-image: linear-gradient(90deg, rgb(232, 147, 204) 0%, rgb(169, 104, 198) 14%, rgb(255, 144, 123) 40%);
-        background-clip: text;
-        -webkit-background-clip: text;
-        text-fill-color: transparent;
-        -webkit-text-fill-color: transparent;
-        font-weight: bold;
-      }
-    }
+  h3 {
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 110%;
+    margin: 20px 0;
+    text-align: left;
     
-    a > div {
-      box-shadow: rgb(57 47 86 / 8%) 0 4px 64px 20px;
-      border-radius: 20px;
+    span {
+      background-color: rgb(232, 147, 204);
+      background-image: linear-gradient(90deg, rgb(232, 147, 204) 0%, rgb(169, 104, 198) 14%, rgb(255, 144, 123) 40%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      text-fill-color: transparent;
+      -webkit-text-fill-color: transparent;
+      font-weight: bold;
     }
+  }
+  
+  a > div {
+    box-shadow: rgb(57 47 86 / 8%) 0 4px 64px 20px;
+    border-radius: 20px;
+  }
+
+  @media screen and (max-width: 700px) {
+    h3 {
+      font-size: 16px;
+    }
+  }
 `
 
 const WorkCard: React.FC<ProjectInterface> = ({ name, shortDescription, linkHref, imageSrc}) => (
